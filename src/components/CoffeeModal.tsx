da09450d2c8194e28aa1e7b40665aac5ef2db6c5
Shanith Perera <shanith.perera@inovaitsys.com>
2022-12-17 20:45:16 +0530

    fix: ui updates

--- src/components/CoffeeModal.tsx
+++ src/components/CoffeeModal.tsx
@@ -26,28 +26,24 @@ export default function CoffeeModal(props: any) {
                     <div
                         className="justify-center items-center flex overflow-x-hidden overflow-y-auto fixed inset-0 z-50 outline-none focus:outline-none"
                     >
-                        <div className="relative w-auto my-6 mx-auto">
+                        <div className="relative w-auto my-6 mx-auto max-w-3xl">
                             {/*content*/}
                             <div className="border-0 rounded-lg shadow-lg relative flex flex-col w-full bg-white outline-none focus:outline-none">
                                 {/*header*/}
                                 <div className="flex items-start justify-between p-5 border-b border-solid border-slate-200 rounded-t">
-                                    <h3 className="break-words text-3xl font-semibold">
+                                    <h3 className="break-all text-3xl font-semibold">
                                         ☕ /{coffee?.name}
                                     </h3>
                                 </div>
                                 {/*body*/}
                                 <div className="relative p-6 flex-auto">
-                                    <div className="my-2 flex flex-col items-center bg-white border-none rounded-lg md:flex-row md:max-w-xl">
-                                        <div className="w-screen flex flex-col justify-between p-4 leading-normal">
-                                            <h5 className="mb-2 text-2xl font-medium tracking-tight text-gray-800">{coffee?.maker ?? ''}</h5>
-                                            <p className="font-normal text-gray-700 dark:text-gray-400">Maker</p>
-                                        </div>
+                                    <div className="block max-w-sm p-4 bg-white border border-gray-200 rounded-lg">
+                                        <h5 className="break-all mb-2 text-2xl font-bold tracking-tight text-gray-800">{coffee?.maker ?? ''}</h5>
+                                        <p className="break-all font-normal text-gray-500">Maker</p>
                                     </div>
-                                    <div className="flex flex-col items-center bg-white border-none rounded-lg md:flex-row md:max-w-xl">
-                                        <div className="w-screen flex flex-col justify-between p-4 leading-normal">
-                                            <h5 className="mb-2 text-2xl font-medium tracking-tight text-gray-800">{coffee?.roast ?? ''}</h5>
-                                            <p className="font-normal text-gray-700 dark:text-gray-400">Roast</p>
-                                        </div>
+                                    <div className="block max-w-sm p-4 my-2 bg-white border border-gray-200 rounded-lg">
+                                        <h5 className="break-all smb-2 text-2xl font-bold tracking-tight text-gray-800">{coffee?.roast ?? ''}</h5>
+                                        <p className="break-all font-normal text-gray-500">Roast</p>
                                     </div>
                                 </div>
                                 {/*footer*/}
