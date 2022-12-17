7953dddde061d2d973b54263d56125de570ae707
Shanith Perera <shanith.perera@inovaitsys.com>
2022-12-17 20:23:47 +0530

    fix: ui changes

--- src/components/CoffeeModal.tsx
+++ src/components/CoffeeModal.tsx
@@ -26,7 +26,7 @@ export default function CoffeeModal(props: any) {
                     <div
                         className="justify-center items-center flex overflow-x-hidden overflow-y-auto fixed inset-0 z-50 outline-none focus:outline-none"
                     >
-                        <div className="relative w-auto my-6 mx-auto max-w-3xl">
+                        <div className="relative w-auto my-6 mx-auto">
                             {/*content*/}
                             <div className="border-0 rounded-lg shadow-lg relative flex flex-col w-full bg-white outline-none focus:outline-none">
                                 {/*header*/}
@@ -37,12 +37,18 @@ export default function CoffeeModal(props: any) {
                                 </div>
                                 {/*body*/}
                                 <div className="relative p-6 flex-auto">
-                                    <span className="text-xs font-semibold inline-block py-1 px-2 uppercase rounded text-neutral-600 bg-neutral-200 uppercase last:mr-0 mr-1">
-                                        {coffee?.maker ?? ''}
-                                    </span>
-                                    <span className="text-xs font-semibold inline-block py-1 px-2 uppercase rounded text-neutral-600 bg-neutral-200 uppercase last:mr-0 mr-1">
-                                        {coffee?.maker ?? ''}
-                                    </span>
+                                    <div className="my-2 flex flex-col items-center bg-white border-none rounded-lg md:flex-row md:max-w-xl">
+                                        <div className="w-screen flex flex-col justify-between p-4 leading-normal">
+                                            <h5 className="mb-2 text-2xl font-medium tracking-tight text-gray-800">{coffee?.maker ?? ''}</h5>
+                                            <p className="font-normal text-gray-700 dark:text-gray-400">Maker</p>
+                                        </div>
+                                    </div>
+                                    <div className="flex flex-col items-center bg-white border-none rounded-lg md:flex-row md:max-w-xl">
+                                        <div className="w-screen flex flex-col justify-between p-4 leading-normal">
+                                            <h5 className="mb-2 text-2xl font-medium tracking-tight text-gray-800">{coffee?.roast ?? ''}</h5>
+                                            <p className="font-normal text-gray-700 dark:text-gray-400">Roast</p>
+                                        </div>
+                                    </div>
                                 </div>
                                 {/*footer*/}
                                 <div className="flex items-center justify-end p-6 border-t border-solid border-slate-200 rounded-b">
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -16,7 +16,7 @@ const Home: NextPage = () => {
       </Head>
       <main className="flex min-h-screen flex-col items-center justify-center bg-white">
         <div className="container flex flex-col items-center justify-center gap-12 px-4 py-16 ">
-          <h1 className="text-5xl font-extrabold tracking-tight text-black sm:text-[5rem]">
+          <h1 className="text-5xl font-bold tracking-tight text-black sm:text-[5rem]">
             Coffee<span className="text-[hsl(15,42%,35%)]">Mate☕</span>
           </h1>
 
